4c081a249d2da1e68c93f2273e1d0a35682bcb99
ferchogmv15 <86312360+ferchogmv15@users.noreply.github.com>
2021-07-17 19:45:24 -0500

    Initial commit

---
+++ app/src/main/java/com/example/pruebanumero/MainActivity.java
@@ -0,0 +1,187 @@
+package com.example.pruebanumero;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.credentials.Credential;
+import com.google.android.gms.auth.api.credentials.HintRequest;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
+    String[] permissions = {
+            Manifest.permission.CAMERA,
+            Manifest.permission.READ_PHONE_STATE
+    };
+    final int PERM_CODE = 11; // entero cualquiera para los permisos
+    GoogleApiClient mGoogleApiClient;
+    private int RESOLVE_HINT = 2;
+    TextView tv;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //checkpermissions();
+        StringBuilder sb = new StringBuilder("El numero telefonico es ");
+
+        //sb.append(mPhoneNumber);
+        tv = findViewById(R.id.tvRespuesta);
+        //tv.setText(sb.toString());
+//        checkpermissions();
+//        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return;
+//        }
+
+        //tv.setText(sb.toString()+ telephonyManager.getImei());
+
+    }
+
+    public void obtenerNumero(View view) {
+        checkpermissions();
+        StringBuilder sb = new StringBuilder("El numero telefonico es ");
+
+        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        sb.append(telephonyManager.getLine1Number());
+        tv.setText(sb.toString());
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void obtenerIMEI(View view) {
+        checkpermissions();
+        StringBuilder sb = new StringBuilder("El IMEI es ");
+
+        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        sb.append(telephonyManager.getImei());
+        tv.setText(sb.toString());
+    }
+
+    public void obtenerNumeroGoogle(View view) {
+        StringBuilder sb = new StringBuilder("El numero telefonico es ");
+
+        //set google api client for hint request
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.CREDENTIALS_API)
+                .build();
+
+        tv.setText(sb.toString());
+
+        // get mobile number from phone
+        getHintPhoneNumber();
+    }
+
+    public void obtenerAndroidID(View view) {
+        StringBuilder sb = new StringBuilder("El Android ID es ");
+
+        // ANDROID ID
+        String androidId = Settings.Secure.getString(getContentResolver(),
+               Settings.Secure.ANDROID_ID);
+        tv.setText(sb.toString()+ androidId);
+    }
+
+    @Override public void onConnected(@Nullable Bundle bundle) {
+    }
+    @Override public void onConnectionSuspended(int i) {
+    }
+    @Override public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+    }
+
+    public void getHintPhoneNumber() {
+        HintRequest hintRequest =
+                new HintRequest.Builder()
+                        .setPhoneNumberIdentifierSupported(true)
+                        .build();
+        PendingIntent mIntent = Auth.CredentialsApi.getHintPickerIntent(mGoogleApiClient, hintRequest);
+        try {
+            startIntentSenderForResult(mIntent.getIntentSender(), RESOLVE_HINT, null, 0, 0, 0);
+        } catch (IntentSender.SendIntentException e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //Result if we want hint number
+        if (requestCode == RESOLVE_HINT) {
+            if (resultCode == Activity.RESULT_OK) {
+                if (data != null) {
+                    Credential credential = data.getParcelableExtra(Credential.EXTRA_KEY);
+                    // credential.getId();  < – will need to process phone number string
+                    tv.append( credential.getId());
+                }
+            }
+        }
+    }
+
+    private boolean checkpermissions(){
+        List<String> listofpermisssions = new ArrayList<>();
+        for (String perm: permissions){
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), perm) != PackageManager.PERMISSION_GRANTED){
+                listofpermisssions.add(perm);
+            }
+        }
+        if (!listofpermisssions.isEmpty()){
+            ActivityCompat.requestPermissions(this, listofpermisssions.toArray(new String[listofpermisssions.size()]), PERM_CODE);
+            return false;
+        }
+        return true;
+    }
+}
